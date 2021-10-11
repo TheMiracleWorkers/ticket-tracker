cde8532395e283d998b2f6fd43c5ec1d38ce2dc0
dmeuwissen <derekmeuwissen@gmail.com>
2021-10-11 18:47:26 +0200

    Add Ticket uses transportlayer

--- src/components/AddTicketForm.tsx
+++ src/components/AddTicketForm.tsx
@@ -7,6 +7,8 @@ import {useFormik} from "formik";
 import axios from "axios";
 import Button from "@mui/material/Button";
 import {Stack} from "@mui/material";
+import Ticket from '../domainObjects/Ticket';
+import {TransportLayer} from '../transportation/TransportLayer';
 
 const validationSchema = yup.object({
     title: yup
@@ -19,6 +21,7 @@ const validationSchema = yup.object({
 });
 
 export default function AddTicketForm() {
+    const transportLayer = new TransportLayer();
     const formik = useFormik({
         initialValues: {
             title: '',
@@ -26,130 +29,133 @@ export default function AddTicketForm() {
         },
         validationSchema: validationSchema,
         onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
-            axios.post('https://derekmeuwissen.pythonanywhere.com/tickets/', values).then(res => {
-                console.log(res)
-            }).catch(err => {
+            const newTicket = new Ticket({
+                'title': values.title,
+                'description': values.description
+            });
+            transportLayer.postTicket(newTicket).catch(err => {
                 console.log(err)
-            })
+            }).then(res =>{
+                // TODO: Close Add ticket form
+            });
         },
     });
 
     return (
         <React.Fragment>
             <form onSubmit={formik.handleSubmit}>
-            <Typography variant="h6" gutterBottom>
-                Add Ticket
-            </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={4}>
-                    <TextField
-                        required
-                        id="title"
-                        name="title"
-                        label="Title"
-                        fullWidth
-                        variant="standard"
-                        value={formik.values.title}
-                        onChange={formik.handleChange}
-                        error={formik.touched.title && Boolean(formik.errors.title)}
-                        helperText={formik.touched.title && formik.errors.title}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <TextField
-                        required
-                        id="project"
-                        name="project"
-                        label="Project"
-                        fullWidth
-                        variant="standard"
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <TextField
-                        required
-                        id="status"
-                        name="status"
-                        label="Status"
-                        fullWidth
-                        variant="standard"
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <TextField
-                        required
-                        id="category"
-                        name="category"
-                        label="Category"
-                        fullWidth
-                        variant="standard"
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12} sm={3}>
-                    <TextField
-                        required
-                        id="due_date"
-                        name="due_date"
-                        label="Due Date"
-                        fullWidth
-                        variant="standard"
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12} sm={1}>
-                    <TextField
-                        required
-                        id="priority"
-                        name="priority"
-                        label="Priority"
-                        fullWidth
-                        variant="standard"
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <TextField
-                        required
-                        id="assigned"
-                        name="assigned"
-                        label="Assigned"
-                        fullWidth
-                        variant="standard"
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12} sm={12}>
-                    <TextField
-                        required
-                        id="description"
-                        name="description"
-                        label="Description"
-                        fullWidth
-                        variant="standard"
-                        multiline
-                        rows={8}
-                        value={formik.values.description}
-                        onChange={formik.handleChange}
-                        error={formik.touched.description && Boolean(formik.errors.description)}
-                        helperText={formik.touched.description && formik.errors.description}
-                    />
-                </Grid>
+                <Typography variant="h6" gutterBottom>
+                    Add Ticket
+                </Typography>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="title"
+                            name="title"
+                            label="Title"
+                            fullWidth
+                            variant="standard"
+                            value={formik.values.title}
+                            onChange={formik.handleChange}
+                            error={formik.touched.title && Boolean(formik.errors.title)}
+                            helperText={formik.touched.title && formik.errors.title}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="project"
+                            name="project"
+                            label="Project"
+                            fullWidth
+                            variant="standard"
+                            disabled
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="status"
+                            name="status"
+                            label="Status"
+                            fullWidth
+                            variant="standard"
+                            disabled
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="category"
+                            name="category"
+                            label="Category"
+                            fullWidth
+                            variant="standard"
+                            disabled
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <TextField
+                            required
+                            id="due_date"
+                            name="due_date"
+                            label="Due Date"
+                            fullWidth
+                            variant="standard"
+                            disabled
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={1}>
+                        <TextField
+                            required
+                            id="priority"
+                            name="priority"
+                            label="Priority"
+                            fullWidth
+                            variant="standard"
+                            disabled
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <TextField
+                            required
+                            id="assigned"
+                            name="assigned"
+                            label="Assigned"
+                            fullWidth
+                            variant="standard"
+                            disabled
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                        <TextField
+                            required
+                            id="description"
+                            name="description"
+                            label="Description"
+                            fullWidth
+                            variant="standard"
+                            multiline
+                            rows={8}
+                            value={formik.values.description}
+                            onChange={formik.handleChange}
+                            error={formik.touched.description && Boolean(formik.errors.description)}
+                            helperText={formik.touched.description && formik.errors.description}
+                        />
+                    </Grid>
 
-                <Grid item xs={12}>
-                    <Stack spacing={2} direction="row">
-                        <Button color="primary" variant="contained" type="submit">
-                            Submit
-                        </Button>
-                        <Button color="primary" variant="outlined">
-                            Cancel
-                        </Button>
-                    </Stack>
+                    <Grid item xs={12}>
+                        <Stack spacing={2} direction="row">
+                            <Button color="primary" variant="contained" type="submit">
+                                Submit
+                            </Button>
+                            <Button color="primary" variant="outlined">
+                                Cancel
+                            </Button>
+                        </Stack>
+                    </Grid>
                 </Grid>
-            </Grid>
             </form>
         </React.Fragment>
     );
