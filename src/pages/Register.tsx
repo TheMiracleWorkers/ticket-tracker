8c01f83b4470d342ecf1b4382102e7b4e178b4a9
nickkersten <cincle@gmail.com>
2021-12-14 14:47:37 +0100

    refactor user registration to transport layer.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,120 +1,151 @@
-import * as React from 'react';
+import * as React from "react";
 
-import {Visibility, VisibilityOff} from '@mui/icons-material';
-import {Button, FormControl, Grid, IconButton, Input, InputAdornment, InputLabel, Typography} from '@mui/material';
-import {Link} from "react-router-dom";
-import PropTypes from 'prop-types';
-import {useFormik} from "formik";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import UserRegistration from "../domainObjects/UserRegistration";
+import { TransportLayer } from "../transportation/TransportLayer";
+import { AxiosError } from "axios";
 
 interface State {
-    showPassword: boolean;
-    username: string;
-    email: string;
-    password: string;
+  showPassword: boolean;
+  username: string;
+  email: string;
+  password: string;
 }
 
-export default function Register(props: { handle_register: (arg0: State) => void; }) {
-    const [values, setValues] = React.useState<State>({
-        showPassword: false,
-        username: '',
-        email: '',
-        password: '',
-    });
+export default function Register(props: { setMessage: Function }) {
+  const [values, setValues] = React.useState<State>({
+    showPassword: false,
+    username: "",
+    email: "",
+    password: "",
+  });
 
-    // Toggle password visibility
-    const handleClickShowPassword = () => {
-        setValues({
-            ...values,
-            showPassword: !values.showPassword,
+  const transportLayer = new TransportLayer();
+
+  // Toggle password visibility
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  // Handle user registration.
+  const handleRegister = (userRegistration: UserRegistration) => {
+    transportLayer
+      .registerUserPromise(userRegistration)
+      .then((response) => {
+        props.setMessage({
+          status: "success",
+          show_message: true,
+          message: "Successfully registered account! Please login",
         });
-    };
+      })
+      .catch((error: AxiosError) => {
+        //Request made an server response.
+        if (error.response) {
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          console.log(error.response.data);
+        }
+      });
+  };
 
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            email: '',
-            password: '',
-        },
-        onSubmit: (values) => {
-            const registerUser = {
-                'showPassword': false,
-                'username': values.username,
-                'email': values.email,
-                'password': values.password,
-            }
-            props.handle_register(registerUser)
-        },
-    });
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      const userRegistration = new UserRegistration(
+        values.username,
+        values.email,
+        values.password
+      );
+      handleRegister(userRegistration);
+    },
+  });
 
-    return (
-        <React.Fragment>
+  return (
+    <React.Fragment>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h1">Register</Typography>
+          </Grid>
 
-            <form onSubmit={formik.handleSubmit}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <Typography variant="h1">Register</Typography>
-                    </Grid>
+          <Grid item xs={12}>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel htmlFor="username">Username</InputLabel>
+              <Input
+                required
+                id="username"
+                value={formik.values.username}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+          </Grid>
 
-                    <Grid item xs={12}>
-                        <FormControl variant="standard" fullWidth>
-                            <InputLabel htmlFor="username">Username</InputLabel>
-                            <Input
-                                required
-                                id="username"
-                                value={formik.values.username}
-                                onChange={formik.handleChange}
-                            />
-                        </FormControl>
-                    </Grid>
+          <Grid item xs={12}>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Input
+                required
+                id="email"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+            </FormControl>
+          </Grid>
 
-                    <Grid item xs={12}>
-                        <FormControl variant="standard" fullWidth>
-                            <InputLabel htmlFor="email">Email</InputLabel>
-                            <Input
-                                required
-                                id="email"
-                                type="email"
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                            />
-                        </FormControl>
-                    </Grid>
+          <Grid item xs={12}>
+            <FormControl variant="standard" fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input
+                required
+                id="password"
+                inputProps={{ minLength: 8 }}
+                type={values.showPassword ? "text" : "password"}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleClickShowPassword}>
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </Grid>
 
-                    <Grid item xs={12}>
-                        <FormControl variant="standard" fullWidth>
-                            <InputLabel htmlFor="password">Password</InputLabel>
-                            <Input
-                                required
-                                id="password"
-                                inputProps={{minLength: 8}}
-                                type={values.showPassword ? 'text' : 'password'}
-                                value={formik.values.password}
-                                onChange={formik.handleChange}
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton onClick={handleClickShowPassword}>
-                                            {values.showPassword ? <VisibilityOff/> : <Visibility/>}
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                            />
-                        </FormControl>
-                    </Grid>
+          <Grid item xs={12} sm={3}>
+            <Button variant="outlined" type="submit" fullWidth>
+              Register
+            </Button>
+          </Grid>
 
-                    <Grid item xs={12} sm={3}>
-                        <Button variant="outlined" type="submit" fullWidth>Register</Button>
-                    </Grid>
-
-                    <Grid item xs={12} sm={9}>
-                        <Button component={Link} to="/">Already have an account?</Button>
-                    </Grid>
-                </Grid>
-            </form>
-        </React.Fragment>
-    )
+          <Grid item xs={12} sm={9}>
+            <Button component={Link} to="/">
+              Already have an account?
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </React.Fragment>
+  );
 }
-
-// Add prop types
-Register.propTypes = {
-    handle_register: PropTypes.func.isRequired
-};
--- src/transportation/TransportLayer.tsx
+++ src/transportation/TransportLayer.tsx
@@ -2,109 +2,119 @@ import axios from "axios";
 import Ticket from "../domainObjects/Ticket";
 import Project from "../domainObjects/Project";
 import inMemoryJWT from "../domainObjects/inMemoryJWTManager";
+import { UserRegistrationInterface } from "../domainObjects/UserRegistration";
 
 export class TransportLayer {
-    apiUrl = process.env.REACT_APP_REST_API;
-    axiosInstance = axios.create({
-        baseURL: this.apiUrl,
-        headers: {
-            Authorization: `JWT ${localStorage.getItem('token')}`
-        }
+  apiUrl = process.env.REACT_APP_REST_API;
+  axiosInstance = axios.create({
+    baseURL: this.apiUrl,
+    headers: {
+      Authorization: `JWT ${localStorage.getItem("token")}`,
+    },
+  });
+
+  getAllTicketsPromise() {
+    return this.axiosInstance.request({
+      method: "GET",
+      url: "/tickets",
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
     });
+  }
 
-    getAllTicketsPromise() {
-        return this.axiosInstance.request({
-            method: "GET",
-            url: "/tickets",
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        });
-    }
+  getTicketByIdPromise(id: number) {
+    return this.axiosInstance.request({
+      method: "GET",
+      url: "/tickets/" + id,
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    getTicketByIdPromise(id: number) {
-        return this.axiosInstance.request({
-            method: "GET",
-            url: "/tickets/" + id,
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        });
-    }
+  postTicket(ticket: Ticket) {
+    return this.axiosInstance.request({
+      method: "POST",
+      url: "/tickets/",
+      data: ticket.toJSON(),
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    postTicket(ticket: Ticket) {
-        return this.axiosInstance.request({
-            method: "POST",
-            url: "/tickets/",
-            data: ticket.toJSON(),
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        })
-    }
+  updateTicketPromise(ticket: Ticket) {
+    return this.axiosInstance.request({
+      method: "PUT",
+      url: "/tickets/" + ticket.id + "/",
+      data: ticket.toJSON(),
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    updateTicketPromise(ticket: Ticket) {
-        return this.axiosInstance.request({
-            method: "PUT",
-            url: "/tickets/" + ticket.id + "/",
-            data: ticket.toJSON(),
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        });
-    }
+  getAllProjectPromise() {
+    return this.axiosInstance.request({
+      method: "GET",
+      url: "/projects",
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    getAllProjectPromise() {
-        return this.axiosInstance.request({
-            method: "GET",
-            url: "/projects",
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        });
-    }
+  getProjectByIdPromise(id: number) {
+    return this.axiosInstance.request({
+      method: "GET",
+      url: "/projects/" + id,
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    getProjectByIdPromise(id: number) {
-        return this.axiosInstance.request({
-            method: "GET",
-            url: "/projects/" + id,
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        });
-    }
+  postProject(project: Project) {
+    return this.axiosInstance.request({
+      method: "POST",
+      url: "/projects/",
+      data: project.toJSON(),
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    postProject(project: Project) {
-        return this.axiosInstance.request({
-            method: "POST",
-            url: "/projects/",
-            data: project.toJSON(),
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        })
-    }
+  updateProjectPromise(project: Project) {
+    return this.axiosInstance.request({
+      method: "PUT",
+      url: "/projects/" + project.id + "/",
+      data: project.toJSON(),
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    updateProjectPromise(project: Project) {
-        return this.axiosInstance.request({
-            method: "PUT",
-            url: "/projects/" + project.id + "/",
-            data: project.toJSON(),
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        });
-    }
+  deleteProject(project: Project) {
+    return this.axiosInstance.request({
+      method: "DELETE",
+      url: "/projects/" + project.id + "/",
+      headers: {
+        Authorization: `JWT ${inMemoryJWT.getToken()}`,
+      },
+    });
+  }
 
-    deleteProject(project: Project) {
-        return this.axiosInstance.request({
-            method: "DELETE",
-            url: "/projects/" + project.id + "/",
-            headers: {
-                Authorization: `JWT ${inMemoryJWT.getToken()}`
-            },
-        })
-       
-    }
-
+  registerUserPromise(userRegistration: UserRegistrationInterface) {
+    return this.axiosInstance.request({
+      method: "POST",
+      url: "/register/",
+      data: JSON.stringify(userRegistration),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
